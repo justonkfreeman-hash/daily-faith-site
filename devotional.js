7bbaa66db479318a168a24829ac18dcd51a7c36a
justonkfreeman-hash <justonkfreeman@gmail.com>
2026-04-16 15:16:19 -0500

    Create devotional.js

---
+++ devotional.js
@@ -0,0 +1,11 @@
+function buildDevotional(verse) {
+  return `Today's verse: ${verse}
+
+God is speaking through this word today. Take a moment to slow down and reflect.
+
+This is a reminder that your situation is not outside of God's control. He is guiding you even when things feel uncertain.
+
+Trust Him today and take one step of faith.`;
+}
+
+module.exports = buildDevotional;
